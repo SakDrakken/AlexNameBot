b171221af1f34ab7cafb306dc4cbca9847f2ac98
SakDrakken <adamkrasa@seznam.cz>
2019-01-16 16:47:17 +0100

    first try

--- AlexNameBot.py
+++ AlexNameBot.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
+import markovify
+
+
 def main():
-    print("ayy")
+    file = open("input2.txt", "r", 8192, "utf-8")
+    text = file.read()
+    text_model = markovify.Text(text)
+    print(text_model.make_sentence(tries=200))
 
 
 if __name__ == '__main__':
