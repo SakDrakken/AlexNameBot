73f09fb01adfd2fc77506b195ed711964463afb5
SakDrakken <adamkrasa@seznam.cz>
2019-01-19 17:28:12 +0100

    small changes

--- AlexNameBot.py
+++ AlexNameBot.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-import markovify
+import keras
 
 
 def main():
-    file = open("input2.txt", "r", 8192, "utf-8")
+    file = open("input.txt", "r", 8192, "utf-8")
     text = file.read()
-    text_model = markovify.Text(text)
-    print(text_model.make_sentence(tries=200))
+    print(text)
 
 
 if __name__ == '__main__':
